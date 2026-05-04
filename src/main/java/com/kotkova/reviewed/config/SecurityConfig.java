6b37d311478f5d403d098408e7fceee3290c16ce
Daniela Kotková <aknad90@gmail.com>
2026-05-04 23:25:51 +0200

    Test 2

--- src/main/java/com/kotkova/reviewed/config/SecurityConfig.java
+++ src/main/java/com/kotkova/reviewed/config/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-asdf
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
